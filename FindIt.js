ca39ccbed92ea9e495814c14ff1de93116054650
Jeff Seaman <jseaman@pointpark.edu>
2019-02-13 19:52:22 -0500

    Added additional files and changes

---
+++ FindIt.js
@@ -0,0 +1,11 @@
+
+var latitude;
+var longitude;
+var parkedLatitude;
+var parkLongitude;
+var storage;
+
+function init(){
+  document.addEventListener("deviceReady",onDeviceReady, false);
+  storage = window.localStorage;
+}
